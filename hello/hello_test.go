d73617d7c5791976c9eebb426062e7d008eb1a47
Brett Horstketter <brehors@outlook.com>
2020-12-20 19:39:36 -0800

    refactors test and adds condition for nil string

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Brett")
-	want := "Hello, Brett"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Brett")
+		want := "Hello, Brett"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("default to 'Hello, world!' if empty string is passed", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world!"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
