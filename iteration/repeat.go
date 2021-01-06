cf9c7615d80b7fa5f1ece22663ce742c22a142f0
Brett Horstketter <brehors@outlook.com>
2021-01-05 20:38:56 -0800

    lecture portion completed

---
+++ iteration/repeat.go
@@ -0,0 +1,11 @@
+package iteration
+
+const repeatCount = 5
+// Repeat the input 5 times
+func Repeat(character string) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
